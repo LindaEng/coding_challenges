d51c55224b42e7643482d87a8d59da5cbbcd7283
= <90lineng@gmail.com>
2022-04-18 12:42:45 -0400

    challenges updated

---
+++ week_3/day_1_challenges.js
@@ -0,0 +1,26 @@
+// Question 1: Is Unique
+// You are given a string. Write a function to determine if all of the characters in the string appear only once.
+
+// Your function shouldn't care about capitalization. A is the same as a.
+
+// Input - "heLlo"
+// Output - False
+
+// Input - "hey"
+// Output - True
+
+
+
+
+//Question 2: Palindrome Permutation: 
+// Given a string, write a function to check if it is a permutation of a palin­ drome. A palindrome is a word or phrase that is the same forwards and backwards. A permutation is a rearrangement of letters. The palindrome does not need to be limited to just dictionary words.
+// EXAMPLE
+// Input: Tact Coa
+// Output: True (permutations: "taco cat", "atco eta", etc.)
+
+
+
+//Question 3: URLify
+// Write a method to replace all spaces in a string with '%20'. You may assume that the string has sufficient space at the end to hold the additional characters,and that you are given the "true" length of the string. (Note: If implementing in Java,please use a character array so that you can perform this operation in place.)
+// EXAMPLE
+// Input: "Mr John Smith ", 13 Output: "Mr%20John%20Smith"
